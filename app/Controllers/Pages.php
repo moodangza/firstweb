d6c654af5f4c819dc56e3c5645419e7fded01866
moodangza <moodangza29@gmail.com>
2023-12-21 15:29:29 +0700

    menu

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -5,12 +5,13 @@ class Pages extends BaseController
 {
     public function index()
     {
+         echo('spicweb/header');
         return view('spicweb/index');
     }
 
     public function view($page = 'hello')
     {
-        return view('spicweb');
+        return view('spicweb/index');
     }
 }
 ?>
---
+++ app/Views/spicweb/header.php
@@ -0,0 +1,16 @@
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Spica Admin</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="firsttemplate/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../../../firsttemplate/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../firsttemplatevendors/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../firsttemplate/images/favicon.png" />
+</head>
